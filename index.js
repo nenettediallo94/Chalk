a3d633755463eef798e28047264147e2ee38b166
nenette <nenettediallo94@gmail.com>
2025-07-10 18:12:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+console.log(chalk.blue('Texte en bleu !'));
+console.log(chalk.red.bold('Texte rouge et gras !'));
+console.log(chalk.green('Ceci est un message en vert.'));
+console.log(chalk.yellow.bgRed('Attention : fond rouge, texte jaune !'));
