7f589cac1610723e4394409bd7c3f576e95e3afb
nenette <nenettediallo94@gmail.com>
2025-07-13 23:30:29 +0200

    projet associé à mon petit package

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import chalk from 'chalk';
+import chalk from 'chalk';     
+import direSalut from 'petitpackage'; 
+
+
 console.log(chalk.blue('Texte en bleu !'));
 console.log(chalk.red.bold('Texte rouge et gras !'));
 console.log(chalk.green('Ceci est un message en vert.'));
 console.log(chalk.yellow.bgRed('Attention : fond rouge, texte jaune !'));
+
+direSalut('Nenette'); 
